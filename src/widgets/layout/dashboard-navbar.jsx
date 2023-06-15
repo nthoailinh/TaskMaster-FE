0b2c0b8678e5960e14ecf7ca1143075005272999
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2023-06-16 00:32:32 +0700

    First commit

---
+++ src/widgets/layout/dashboard-navbar.jsx
@@ -0,0 +1,123 @@
+import { useState, useEffect } from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+
+import {
+  PlusIcon,
+  ArrowsRightLeftIcon,
+  UserIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+
+export function DashboardNavbar() {
+  const [openNav, setOpenNav] = useState(false);
+  const [activeSection, setActiveSection] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a
+          href="#"
+          className={`flex items-center ${
+            activeSection === "overview"
+              ? "text-lightBlue-500"
+              : "text-blueGray-500"
+          }`}
+          onMouseEnter={() => setActiveSection("overview")}
+          onMouseLeave={() => setActiveSection(null)}
+        >
+          Tổng quan
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a
+          href="#"
+          className={`flex items-center ${
+            activeSection === "personal"
+              ? "text-lightBlue-500"
+              : "text-blueGray-500"
+          }`}
+          onMouseEnter={() => setActiveSection("personal")}
+          onMouseLeave={() => setActiveSection(null)}
+        >
+          Cá nhân
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a
+          href="#"
+          className={`flex items-center ${
+            activeSection === "group"
+              ? "text-lightBlue-500"
+              : "text-blueGray-500"
+          }`}
+          onMouseEnter={() => setActiveSection("group")}
+          onMouseLeave={() => setActiveSection(null)}
+        >
+          Nhóm
+        </a>
+      </Typography>
+    </ul>
+  );
+
+  return (
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+        <div className="hidden lg:block">{navList}</div>
+        <div className="ml-auto flex items-center">
+          <Button color="purple" className="mr-5 flex items-center">
+            <ArrowsRightLeftIcon className="text-500 mr-1 h-5 w-5" />
+            Tích hợp
+          </Button>
+          <Button className="mr-2 flex items-center">
+            <PlusIcon className="text-500 mr-1 h-5 w-5" />
+            Thêm việc
+          </Button>
+        </div>
+      </div>
+      <MobileNav open={openNav}>
+        <div className="ml-auto flex items-center">
+          <Button color="purple" className="mr-5 flex items-center">
+            <ArrowsRightLeftIcon className="text-500 mr-1 h-5 w-5" />
+            Tích hợp
+          </Button>
+          <Button className="mr-2 flex items-center">
+            <PlusIcon className="text-500 mr-1 h-5 w-5" />
+            Thêm việc
+          </Button>
+        </div>
+      </MobileNav>
+    </Navbar>
+  );
+}
+
+DashboardNavbar.displayName = "/src/widgets/layout/dashboard-navbar.jsx";
+
+export default DashboardNavbar;
