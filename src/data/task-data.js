347365c1563c372a48a3fad4988b0c90cb0e6dac
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2023-06-16 20:16:49 +0700

    Update Các công việc còn lại

---
+++ src/data/task-data.js
@@ -0,0 +1,44 @@
+export const CardData = [
+  {
+    color: "blue",
+    title: "Vẽ nguyên mẫu giao diện",
+    tag: 'UIUX',
+    footer: {
+      value: "15:00 AM 02/06/2023",
+    },
+  },
+  {
+    color: "pink",
+    title: "Code dự án ACR",
+    tag: 'ACR',
+    footer: {
+      value: "16:00 AM 05/06/2023",
+    },
+  },
+  {
+    color: "green",
+    title: "AWS Spot Instance",
+    tag: 'AWS',
+    footer: {
+      value: "17:00 AM 10/06/2023",
+    },
+  },
+  {
+    color: "orange",
+    title: "Họp dự án ACR",
+    tag: 'ACR',
+    footer: {
+      value: "11:00 AM 08/06/2023",
+    },
+  },
+  {
+    color: "red",
+    title: "Code dự án Vbee",
+    tag: 'ACR',
+    footer: {
+      value: "16:00 AM 05/06/2023",
+    },
+  },
+];
+
+export default CardData;
