81ec07e2b3e45296f57b384bf1ec56d56329532a
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2023-06-22 10:07:41 +0700

    Update

--- src/widgets/popup/AddTaskPopup.jsx
+++ src/widgets/popup/AddTaskPopup.jsx
@@ -15,7 +15,202 @@ const piorityOptions = [
   "Không quan trọng - Không khẩn cấp",
 ];
 
-export function AddTaskPopup() {
+function TaskNameInput({ value, onChange }) {
+  return (
+    <div className="mt-8">
+      <Typography className="font-normal">Tên công việc</Typography>
+      <Input
+        className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+        labelProps={{
+          className: "hidden",
+        }}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+function DescriptionInput({ value, onChange }) {
+  return (
+    <div className="mt-3">
+      <Typography className="font-normal">Mô tả công việc</Typography>
+      <Input
+        className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+        labelProps={{
+          className: "hidden",
+        }}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+function DateTimeInputs({
+  startTimeDay,
+  startTimeHour,
+  endTimeHour,
+  deadlineDay,
+  deadlineHour,
+  handleStartTimeDayChange,
+  handleStartTimeHourChange,
+  handleEndTimeHourChange,
+  handleDeadlineDayChange,
+  handleDeadlineHourChange,
+}) {
+  return (
+    <div className="mt-8">
+      <Typography className="font-normal">Thực hiện</Typography>
+      <div className="flex space-x-20">
+        <div className="w-32 mr-2">
+          <Input
+            className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+            labelProps={{
+              className: "hidden",
+            }}
+            type="date"
+            value={startTimeDay}
+            onChange={handleStartTimeDayChange}
+          />
+        </div>
+        <div className="w-32">
+          <Input
+            className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+            labelProps={{
+              className: "hidden",
+            }}
+            type="time"
+            value={startTimeHour}
+            onChange={handleStartTimeHourChange}
+          />
+        </div>
+        <div className="w-32">
+          <Input
+            className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+            labelProps={{
+              className: "hidden",
+            }}
+            type="time"
+            value={endTimeHour}
+            onChange={handleEndTimeHourChange}
+          />
+        </div>
+      </div>
+      <div className="mt-3">
+        <Typography className="font-normal">Đến hạn</Typography>
+        <div className="flex space-x-20">
+          <div className="w-32 mr-2">
+            <Input
+              className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+              labelProps={{
+                className: "hidden",
+              }}
+              type="date"
+              value={deadlineDay}
+              onChange={handleDeadlineDayChange}
+            />
+          </div>
+          <div className="w-32">
+            <Input
+              className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+              labelProps={{
+                className: "hidden",
+              }}
+              type="time"
+              value={deadlineHour}
+              onChange={handleDeadlineHourChange}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function PrioritySelect({ selected, handlePriorityChange }) {
+  return (
+    <div className="w-96">
+      <Listbox value={selected} onChange={handlePriorityChange}>
+        <Listbox.Label>
+          <Typography className="font-normal">Độ ưu tiên</Typography>
+        </Listbox.Label>
+        <div className="mt-1 relative">
+          <Listbox.Button className="w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
+            <span className="block truncate">{selected}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <ChevronUpDownIcon
+                className="h-4 w-4 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+              {piorityOptions.map((option) => (
+                <Listbox.Option
+                  key={option}
+                  value={option}
+                  className={({ active }) =>
+                    `${active ? "text-blue-900 bg-blue-100" : "text-gray-900"}
+                      cursor-default select-none relative py-2 pl-3 pr-9`
+                  }
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`${
+                          selected ? "font-medium" : "font-normal"
+                        } block truncate`}
+                      >
+                        {option}
+                      </span>
+
+                      {selected && (
+                        <span
+                          className={`${
+                            active ? "text-blue-600" : "text-blue-600"
+                          }
+                            absolute inset-y-0 right-0 flex items-center pr-4`}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
+
+function TagInput({ value, onChange }) {
+  return (
+    <div className="mt-0.5">
+      <Typography className="font-normal">Tag</Typography>
+      <Input
+        className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
+        labelProps={{
+          className: "hidden",
+        }}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+function AddTaskPopup() {
   const [isOpen, setIsOpen] = useState(false);
   const [taskName, setTaskName] = useState("");
   const [description, setDescription] = useState("");
@@ -26,6 +221,7 @@ export function AddTaskPopup() {
   const [deadlineHour, setDeadlineHour] = useState("");
   const [selected, setSelected] = useState(piorityOptions[0]);
   const [tag, setTag] = useState("");
+  const [notification, setNotification] = useState(false);
 
   function closeModal() {
     setIsOpen(false);
@@ -128,199 +324,60 @@ export function AddTaskPopup() {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6 pb-8 text-left align-middle shadow-xl transition-all">
-                  <div className="flex justify-end">
-                    <button
-                      className="text-gray-500 hover:text-gray-600"
+                  <div className="flex justify-between">
+                    <Typography variant="h3" color="gray" className="pt-12">
+                      Thêm công việc cá nhân
+                    </Typography>
+                    <XMarkIcon
                       onClick={closeModal}
+                      className="h-6 w-6 text-gray-400 cursor-pointer"
+                    />
+                  </div>
+
+                  <TaskNameInput
+                    value={taskName}
+                    onChange={handleTaskNameChange}
+                  />
+                  <DescriptionInput
+                    value={description}
+                    onChange={handleDescriptionChange}
+                  />
+                  <DateTimeInputs
+                    startTimeDay={startTimeDay}
+                    startTimeHour={startTimeHour}
+                    endTimeHour={endTimeHour}
+                    deadlineDay={deadlineDay}
+                    deadlineHour={deadlineHour}
+                    handleStartTimeDayChange={handleStartTimeDayChange}
+                    handleStartTimeHourChange={handleStartTimeHourChange}
+                    handleEndTimeHourChange={handleEndTimeHourChange}
+                    handleDeadlineDayChange={handleDeadlineDayChange}
+                    handleDeadlineHourChange={handleDeadlineHourChange}
+                  />
+                  <PrioritySelect
+                    selected={selected}
+                    handlePriorityChange={handlePriorityChange}
+                  />
+                  <TagInput value={tag} onChange={handleTagChange} />
+
+                  <div className="mt-4 flex justify-start">
+                    <Typography className="mr-8 font-normal">
+                      Bật thông báo
+                    </Typography>
+                    <Switch
+                      checked={notification}
+                      className={`${
+                        false ? "bg-blue-600" : "bg-gray-200"
+                      } relative inline-flex items-center h-6 rounded-full w-11`}
                     >
-                      <XMarkIcon className="h-5 w-5" />
-                    </button>
-                  </div>
-                  <Dialog.Title
-                    as="h3"
-                    className="text-3xl font-medium leading-6 text-gray-900 pt-8"
-                  >
-                    Thêm công việc cá nhân
-                  </Dialog.Title>
-                  <div className="mt-12">
-                    <Typography className="font-normal">
-                      Tên công việc
-                    </Typography>
-                    <Input
-                      className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                      labelProps={{
-                        className: "hidden",
-                      }}
-                      value={taskName}
-                      onChange={handleTaskNameChange}
-                    />
-                  </div>
-                  <div className="mt-3">
-                    <Typography className="font-normal">
-                      Mô tả công việc
-                    </Typography>
-                    <Input
-                      className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                      labelProps={{
-                        className: "hidden",
-                      }}
-                      value={description}
-                      onChange={handleDescriptionChange}
-                    />
-                  </div>
-                  <div className="mt-8">
-                    <Typography className="font-normal">Thực hiện</Typography>
-                    <div className="flex space-x-20">
-                      <div className="w-32 mr-2">
-                        <Input
-                          className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                          labelProps={{
-                            className: "hidden",
-                          }}
-                          type="date"
-                          value={startTimeDay}
-                          onChange={handleStartTimeDayChange}
-                        />
-                      </div>
-                      <div className="w-32">
-                        <Input
-                          className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                          labelProps={{
-                            className: "hidden",
-                          }}
-                          type="time"
-                          value={startTimeHour}
-                          onChange={handleStartTimeHourChange}
-                        />
-                      </div>
-                      <div className="w-32">
-                        <Input
-                          className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                          labelProps={{
-                            className: "hidden",
-                          }}
-                          type="time"
-                          value={endTimeHour}
-                          onChange={handleEndTimeHourChange}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="mt-3">
-                    <Typography className="font-normal">Đến hạn</Typography>
-                    <div className="flex space-x-20">
-                      <div className="w-32 mr-2">
-                        <Input
-                          className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                          labelProps={{
-                            className: "hidden",
-                          }}
-                          type="date"
-                          value={deadlineDay}
-                          onChange={handleDeadlineDayChange}
-                        />
-                      </div>
-                      <div className="w-32">
-                        <Input
-                          className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                          labelProps={{
-                            className: "hidden",
-                          }}
-                          type="time"
-                          value={deadlineHour}
-                          onChange={handleDeadlineHourChange}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="mt-8 flex space-x-10">
-                    <div className="w-96">
-                      <Listbox value={selected} onChange={handlePriorityChange}>
-                        <Listbox.Label>
-                          <Typography className="font-normal">
-                            Độ ưu tiên
-                          </Typography>
-                        </Listbox.Label>
-                        <div className="mt-1 relative">
-                          <Listbox.Button className="w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 sm:text-sm">
-                            <span className="block truncate">{selected}</span>
-                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                              <ChevronUpDownIcon
-                                className="h-4 w-4 text-gray-400"
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </Listbox.Button>
-
-                          <Transition
-                            as={Fragment}
-                            leave="transition ease-in duration-100"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                          >
-                            <Listbox.Options className="absolute mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                              {piorityOptions.map((option) => (
-                                <Listbox.Option
-                                  key={option}
-                                  value={option}
-                                  className={({ active }) =>
-                                    `${
-                                      active
-                                        ? "text-blue-900 bg-blue-100"
-                                        : "text-gray-900"
-                                    }
-                                    cursor-default select-none relative py-2 pl-3 pr-9`
-                                  }
-                                >
-                                  {({ selected, active }) => (
-                                    <>
-                                      <span
-                                        className={`${
-                                          selected
-                                            ? "font-medium"
-                                            : "font-normal"
-                                        } block truncate`}
-                                      >
-                                        {option}
-                                      </span>
-
-                                      {selected && (
-                                        <span
-                                          className={`${
-                                            active
-                                              ? "text-blue-600"
-                                              : "text-blue-600"
-                                          }
-                                          absolute inset-y-0 right-0 flex items-center pr-4`}
-                                        >
-                                          <CheckIcon
-                                            className="h-5 w-5"
-                                            aria-hidden="true"
-                                          />
-                                        </span>
-                                      )}
-                                    </>
-                                  )}
-                                </Listbox.Option>
-                              ))}
-                            </Listbox.Options>
-                          </Transition>
-                        </div>
-                      </Listbox>
-                    </div>
-
-                    <div className="mt-0.5">
-                      <Typography className="font-normal">Tag</Typography>
-                      <Input
-                        className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
-                        labelProps={{
-                          className: "hidden",
-                        }}
-                        value={tag}
-                        onChange={handleTagChange}
+                      <span
+                        className={`${
+                          false ? "translate-x-6" : "translate-x-1"
+                        } inline-block w-4 h-4 transform bg-white rounded-full`}
                       />
-                    </div>
+                    </Switch>
                   </div>
+
                   <div className="flex justify-center mt-8 pt-8">
                     <Button
                       onClick={handleSave}
