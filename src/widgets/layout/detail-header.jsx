904cf12c1bcec13bdc4bbc6f7695a70c5237ac98
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2023-06-24 17:29:01 +0700

    Update

--- src/widgets/layout/detail-header.jsx
+++ src/widgets/layout/detail-header.jsx
@@ -1,23 +1,18 @@
 import React, { useState } from "react";
-import {
-  Card,
-  CardHeader,
-  Tabs,
-  TabsHeader,
-  Tab,
-  Typography,
-} from "@material-tailwind/react";
-import { NavLink } from "react-router-dom";
+import { Card, CardHeader, Button, Typography } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 import routes from "@/routes";
 import { StarIcon, NewspaperIcon } from "@heroicons/react/24/outline";
 
 export function DetailHeader({ completedTask, newTask }) {
-  const [activeTab, setActiveTab] = useState(0);
-
-  const handleTabChange = (index) => {
-    setActiveTab(index);
+  const [selectedTab, setSelectedTab] = useState("personal");
+  const navigate = useNavigate();
+  function handleClick(path) {
+    navigate(path);
+  }
+  const handleTabChange = (tab) => {
+    setSelectedTab(tab);
   };
-
   return (
     <div className="mt-4">
       <div className="mb-6 grid grid-cols-1">
@@ -29,49 +24,39 @@ export function DetailHeader({ completedTask, newTask }) {
             className="m-0 flex items-center justify-between p-6"
           >
             <div className="col-span-1 flex items-center justify-center ml-4">
-              <Tabs value={activeTab}>
-                <TabsHeader
-                  indicatorProps={{
-                    className: "bg-blue-50 shadow-none text-blue-500",
-                  }}
-                >
-                  {routes.map(
-                    ({ layout, pages }) =>
-                      layout === "dashboard" &&
-                      pages.map(
-                        ({ label, path }, key) =>
-                          path.includes("/detail") && (
-                            <NavLink
-                              to={`/${layout}${path}`}
-                              className="px-2"
-                              key={key}
-                            >
-                              <Tab
-                                value={key}
-                                className="py-4 px-2"
-                                onClick={() => handleTabChange(key)}
-                              >
-                                {label}
-                              </Tab>
-                            </NavLink>
-                          )
+              {routes.map(
+                ({ layout, pages }) =>
+                  layout === "dashboard" &&
+                  pages.map(
+                    ({ label, path }, key) =>
+                      path.includes("/detail") && (
+                        <Button
+                          variant="text"
+                          value={key}
+                          onClick={() => handleClick(`/${layout}${path}`)}
+                        >
+                          {label}
+                        </Button>
                       )
-                  )}
-                </TabsHeader>
-              </Tabs>
+                  )
+              )}
             </div>
             <div className="col-span-1 flex items-center justify-center">
               <Typography className="text-lg">
                 <StarIcon className="h-6 w-6 inline-block mr-2" />
                 Nhiệm vụ đã hoàn thành
-                <span className="font-bold ml-8">{completedTask.toString().padStart(2, '0')}</span>
+                <span className="font-bold ml-8">
+                  {completedTask.toString().padStart(2, "0")}
+                </span>
               </Typography>
             </div>
             <div className="col-span-1 flex items-center justify-center mr-8">
               <Typography className="text-lg">
                 <NewspaperIcon className="h-6 w-6 inline-block mr-2" />
                 Nhiệm vụ mới
-                <span className="font-bold ml-8">{newTask.toString().padStart(2, '0')}</span>
+                <span className="font-bold ml-8">
+                  {newTask.toString().padStart(2, "0")}
+                </span>
               </Typography>
             </div>
           </CardHeader>
