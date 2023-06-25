c55decfed83d3da3a77c7f99c7574e991d9cd1da
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2023-06-25 11:58:46 +0700

    Update

--- src/widgets/popup/components/TagInput.jsx
+++ src/widgets/popup/components/TagInput.jsx
@@ -3,7 +3,7 @@ import { Typography, Input } from "@material-tailwind/react";
 export function TagInput({ value, onChange }) {
   return (
     <div className="mt-0.5">
-      <Typography className="font-normal ">Tag</Typography>
+      <Typography className="font-normal ">Nơi làm việc</Typography>
       <Input
         className="focus:!border-t-blue-500 focus:!border-blue-500 !border !border-blue-gray-100 "
         labelProps={{
